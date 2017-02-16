67a3c5c10ba80f6637eeb35ded5797b62b75e167
denislicau <denisl.pericol@gmail.com>
2017-02-16 11:44:14 +0100

    prepared server socket

---
+++ TheService/TheService/Socket/ClientCommunication.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TheService
+{
+    class ClientCommunication
+    {
+        private Socket client;		// socket to client
+
+        // Constructor
+        public ClientCommunication(Socket client)
+        {
+            this.client = client;
+        }
+
+        // Thread method, which communicates with a client
+        public void HandleClient()
+        {
+            string data;
+            NetworkStream networkstream;
+            StreamReader streamreader;
+            StreamWriter streamwriter;
+
+            networkstream = new NetworkStream(this.client);
+            streamreader = new StreamReader(networkstream);
+            streamwriter = new StreamWriter(networkstream);
+
+            data = "Welcome to the Echo Server. Write a text:";
+            streamwriter.WriteLine(data);		// Write greeting to Client
+            streamwriter.Flush();
+
+            while (true)		// while communicating with Client
+            {
+                try
+                {
+                    data = streamreader.ReadLine();	// Read data from Client
+                }
+                catch (IOException)		// The Client has closed the connection
+                {
+                    break;
+                }
+
+                //Console.WriteLine(data);
+                if (data == "DOIT")
+                {
+                    string stringToSend = "I WILL DO IT";
+                    streamwriter.WriteLine(stringToSend);
+                }
+                streamwriter.Flush();
+            }
+            //Console.WriteLine("Connection to client is closing down...\n");
+            streamwriter.Close();
+            streamreader.Close();
+            networkstream.Close();
+        }
+    }
+}
---
+++ TheService/TheService/Socket/SocketController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TheService
+{
+    class SocketController
+    {
+        private const int PORT = 12345;
+        IPEndPoint port;
+            Thread clientThread;
+            ClientCommunication clientCommunication;
+            ThreadStart threadDelegate;
+            Socket clientSocket;				// socket to client
+        public SocketController(){
+            port = new IPEndPoint(IPAddress.Any, PORT);
+            //start the server once with the initialization of the class..we may change that
+            RunServer();
+        }
+
+        private void RunServer(){
+            //create the socket and place it in a listening state
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(port);
+            listener.Listen(10);
+            //run the server. we may need to find better ways to handle this..like being able to stop it and start it by command
+            while (true)			// Endless loop for Server
+            {
+                //every console.WriteLine should be replaced to a writeline to a log file, so we can see what happens in the service
+                //Console.WriteLine("Waiting for a client...");
+
+                clientSocket = listener.Accept();		// await connection from client
+                //Console.WriteLine("Client connected...");
+
+                // Preparation and start of thread which communicates with client
+                clientCommunication = new ClientCommunication(clientSocket);
+                threadDelegate = new ThreadStart(clientCommunication.HandleClient);
+                clientThread = new Thread(threadDelegate);
+                clientThread.Start();	// start client thread
+            }
+
+        }
+
+            
+
+            
+
+    }
+}
+
